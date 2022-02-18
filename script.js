9920d6186810232b6d997537f822cd37688dc45c
Fernando Venturini <fernandoventurini9@hotmail.com>
2022-02-18 00:34:31 +0000

    Criado todas as seções

---
+++ script.js
@@ -0,0 +1,22 @@
+$('.container-slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    centerMode: true,
+    centerPadding: 0,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: false,
+    responsive:[
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ],
+});
